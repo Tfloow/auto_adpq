bb201d5e615a21247c516c8440dc8a640a2cb3bc
Tfloow <88899420+Tfloow@users.noreply.github.com>
2025-11-30 15:38:22 +0100

    New hotfix

--- src/auto_adpq/module.py
+++ src/auto_adpq/module.py
@@ -135,9 +135,11 @@ class AdpQQuantizedWeights:
 
         if self.zeropoint is not None:
             # Meaning it is an array which is not none, can have np.array(None)
-            if type(self.zeropoint) == np.ndarray and self.zeropoint.ndim != 0: 
+            if type(self.zeropoint) is np.ndarray and self.zeropoint.ndim != 0:
                 if len(self.zeropoint) != self.group_num:
                     raise ValueError("Dimensions mismatch for zeropoint")
+            elif len(self.zeropoint) != self.group_num:
+                raise ValueError("Dimensions mismatch for zeropoint")
 
 
 class Auto_AdpQ:
