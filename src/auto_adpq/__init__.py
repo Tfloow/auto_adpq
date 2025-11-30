c7e57755b00aa854e1d68da44ef701495b3b29b2
Tfloow <88899420+Tfloow@users.noreply.github.com>
2025-11-30 15:32:31 +0100

    Hotfix version

--- src/auto_adpq/__init__.py
+++ src/auto_adpq/__init__.py
@@ -10,7 +10,7 @@ A brief description of what your package does.
 from .module import AdpQQuantizedWeights, Auto_AdpQ, AutoAdpQConfig
 
 # Define the package version
-__version__ = "0.2.1"
+__version__ = "0.3.1"
 
 # List of names to expose when a user does `from auto_adpq import *`
 __all__ = ["Auto_AdpQ", "AutoAdpQConfig", "AdpQQuantizedWeights", "__version__"]
